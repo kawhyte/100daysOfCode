0e5c95e323afe4de83b23cdba0b6b5ef0981261b
Kenny <kawhyte@gmail.com>
2020-01-12 19:02:09 -0800

    HTML Layout

--- Projects /VideoGameOfTheYear/index.js
+++ Projects /VideoGameOfTheYear/index.js
@@ -59,10 +59,9 @@ function switchGray() {
 }
 
 // const gallery = document.querySelector('.gallery');
-let html2 = "";
 
-const gallery = document.querySelector(".test");
-const Title = document.querySelector(".clip");
+const gallery = document.querySelector(".game-gallery");
+
 //     const overlay = document.querySelector('.overlay');
 //     const overlayImage = overlay.querySelector('img');
 //     const overlayClose = overlay.querySelector('.close');
@@ -71,7 +70,7 @@ function generateHTML(data) {
   console.log("inside generate " + data.gameName);
   return `
 
-      <div class="fl w-50 w-25-m w-20-l pa2">
+      <div class="fl w-50 w-25-m w-25-l pa2">
       <a href="" class="db link dim tc">
         <img src=${data.photolink} alt="Santigold 99 cents - Album cover" class="w-100 db outline black-10"/>
         <dl class="mt2 f6 lh-copy">
@@ -94,31 +93,7 @@ const data = [
   { photolink:"http://is2.mzstatic.com/image/thumb/Music18/v4/34/03/34/34033451-12e2-2d0b-c100-11a390922a01/source/400x40000bb.png", gameName: "Mario Brothers 2", year: "1997" }
  
 ];
-//var arr = ["cmm", "com", "cng"];
 
-// for (let i = 0; i < data.Roles.length; i++) {
-//   const el = data.Roles[i];
-//  //html2 +=
-//   //console.log(el)
-//   ///if (arr.indexOf(el.code) > -1) out.push(el.fullname);
-// }
-
-//  <div class="item h${1} v${4}">
-
-//         <img src="images/${1}.jpg">
-//         <div class="item__overlay">
-//           <button>View →</button>
-//         </div>
-//       </div>
-
-//   function randomNumber(limit) {
-//     return Math.floor(Math.random() * limit) + 1;
-//   }
-//data.map(generateHTML)
-// const digits = Array.from({ length: 50 }, () => [4, 4]).concat([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
-//   const digits = Array.from(data)
-//   console.log(data)
-//  console.log(digits);
 const html = data.map(generateHTML).join("");
-//console.log(html);
+
 gallery.innerHTML = html;
