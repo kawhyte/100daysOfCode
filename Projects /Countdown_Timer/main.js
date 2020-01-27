f7acbfe372f89824a124b085721796f56128c0ec
Kenny <kawhyte@gmail.com>
2020-01-26 18:25:31 -0800

    added reset

--- Projects /Countdown_Timer/main.js
+++ Projects /Countdown_Timer/main.js
@@ -1,23 +1,69 @@
-const startTime = 25;
-let time =  startTime * 60;
+let startTime = .1;
+let time = startTime * 60;
+// let isPaused = false;
+let isStarted = false;
+var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3');
 
-const startTimerBtn =  document.getElementById('get_Timer')
 
-const countdownElement =  document.getElementById("timer")
+const startTimerBtn = document.getElementById("start_Timer");
+const resetTimerBtn = document.getElementById("reset_Timer");
+// const pauseTimerBtn = document.getElementById("pause_Timer");
 
+const countdownElement = document.getElementById("timer");
+let interval = 0;
+//let set = setInterval(updateCountdown, 1000);
 
-startTimerBtn.addEventListener('click', ()=>{
+startTimerBtn.addEventListener("click", () => {
+  console.log("Is Started", isStarted);
 
-    setInterval(updateCountdown, 1000);
+  if (isStarted === true) {
+    return;
+  }
+
+  isStarted = true;
+  interval = setInterval(updateCountdown, 1000);
 });
 
- 
-function updateCountdown(){
+// pauseTimerBtn.addEventListener("click", () => {
+//   isPaused = isPaused === false ? true : false;
+//   console.log("is pasued", isPaused);
+//   console.log("current Time", countdownElement.innerHTML);
+//   let temp = countdownElement.innerHTML;
 
-const minutes = Math.floor(time/60);
-let seconds = time % 60
-seconds =  seconds < 10 ? "0" + seconds : seconds
+//   if (isPaused) {
+//     clearInterval(interval);
+//   } else {
+//     countdownElement.innerHTML = temp;
+//     setInterval(updateCountdown, 1000);
+//   }
+// });
 
-countdownElement.innerHTML = `${minutes}:${seconds}`
-time--;
+resetTimerBtn.addEventListener("click", () => {
+  clearInterval(interval);
+  startTime = 25;
+  countdownElement.innerHTML = `25:00`;
+  isStarted = false;
+});
+
+function updateCountdown() {
+  console.log("time", time);
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  countdownElement.innerHTML = `${minutes}:${seconds}`;
+  time--;
+
+  isStarted === true;
+  console.log("current time", countdownElement.innerHTML);
+  if (time <= 0) {
+    //audio.play();
+   
+    location.reload();
+    // console.log("Zero", countdownElement.innerHTML);
+    // countdownElement.innerHTML = `25:00`;
+    // clearInterval(interval);
+    // isStarted = false;
+    // startTime = 25;
+  }
 }
